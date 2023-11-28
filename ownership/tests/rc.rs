24b812e34c967a43086f8b4af15f779d5fa6bd24
Robert <robert.nolte@andrena.de>
2023-11-28 13:53:15 +0100

    Hints

--- ownership/tests/rc.rs
+++ ownership/tests/rc.rs
@@ -2,3 +2,4 @@ use spectral::option::OptionAssertions;
 use std::rc::Rc;
 
 use spectral::assert_that;
+//you could implement tests covering Rc here
--- ownership/tests/refcell.rs
+++ ownership/tests/refcell.rs
@@ -1,2 +1,3 @@
 use spectral::assert_that;
 use std::cell::RefCell;
+//you could implement tests covering RefCell here
