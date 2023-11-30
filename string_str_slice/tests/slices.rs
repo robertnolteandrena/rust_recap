f8876ac273baf9098364fc493f704889ff9b8045
Robert <robert.nolte@andrena.de>
2023-11-30 17:02:27 +0100

    Slicing

--- string_str_slice/tests/slices.rs
+++ string_str_slice/tests/slices.rs
@@ -15,6 +15,7 @@ fn taking_a_slice_from_a_string() {
     assert_that!(hello).is_equal_to("hello");
     assert_that!(world).is_equal_to("world");
 }
+
 #[test]
 fn slicing_utf8() {
     let hello_world = "Привіт світ";
